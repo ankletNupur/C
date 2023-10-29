693db0dbfcdc5e76e5d370ec271d86503ea9463b
Nupur Singh <ankletnupur16@gmail.com>
2023-10-29 13:14:58 +0530

    changes done

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main() {
+   // printf() displays the string inside quotation
+   int a;
+   int b;
+   int sum;
+
+   printf("Input first number\n");
+   scanf("%d", &a);
+   printf("Input Second number\n");
+   scanf("%d", &b);
+   sum = a + b;
+   printf("Sum is %d\n", sum);
+} 
